23379de1b02373488931e48be15ddec3e97d8824
Kuhandran <skuhandran@yahoo.com>
2021-07-23 22:44:21 +0800

    updated with login page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Tdcx Assigment
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Card className="Login-cards">
+          <Card.Body>
+            <Card.Title className="Login-cards-title" >Login</Card.Title>
+            <Form>
+              <Form.Group className="mb-3">
+                <Form.Control className="Login-cards-Text" type="text" placeholder="id" />
+              </Form.Group>
+
+              <Form.Group className="mb-3" >
+                <Form.Control className="Login-cards-Text" type="text" placeholder="name" />
+              </Form.Group>
+              
+              <Button variant="primary" className="Login-cards-Submit" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Card.Body>
+        </Card>
       </header>
     </div>
   );
